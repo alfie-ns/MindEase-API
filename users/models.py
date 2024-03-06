93470473d15e5c0b34d3cfa999cd1096908ab720
alfie-ns <alfienurse@gmail.com>
2024-03-06 22:43:10 +0000

    update

--- users/models.py
+++ users/models.py
@@ -1,11 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 
-class User(models.Model):
+class UserProfile(models.Model):
 
     # Choices
     GENDER_CHOICES = [
@@ -28,9 +27,33 @@ class User(models.Model):
     ]
 
     # Fields
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    brain_injury_details = models.TextField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
+    goal = models.CharField(max_length=50, choices=GOAL_CHOICES, blank=True)
+    determination_level = models.CharField(max_length=20, choices=DETERMINATION_LEVEL_CHOICES, blank=True)
 
-    username = models.CharField(max_length=255, unique=True) # must be unique an < 255 characters
-    email = models.EmailField(max_length=255, unique=True) # must be unique an < 255 characters
-    brain_injury_details = models.TextField(null=True, blank=True) # can be null and blank
-    created_at = models.DateTimeField(auto_now_add=True) 
+    # Methods
 
+    def __str__(self):
+        return self.user.username
+
+    # Create or update user profile
+    @receiver(post_save, sender=User)
+    def create_or_update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+        else:
+            instance.userprofile.save()
+
+    # Send welcome email on user creation
+    @receiver(post_save, sender=User)
+    def send_welcome_email(sender, instance, created, **kwargs):
+        if created:
+            send_mail(
+                'Welcome to MindEase!',
+                f"Hi {instance.username}, welcome to MindEase! We're excited to have you onboard.",
+                'from@example.com',
+                [instance.email],
+                fail_silently=False,
+            )
