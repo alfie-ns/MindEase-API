ffe0bd20dfd53f41236a89edae315d4f1ad7f32e
alfie-ns <alfienurse@gmail.com>
2024-03-06 16:10:40 +0000

    update

--- users/models.py
+++ users/models.py
@@ -1,7 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
-class User(AbstractBaseUser):
+class User(models.Model):
+
+    # Choices
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    GOAL_CHOICES = [
+        ('improve_mental_health', 'Improve Mental Health'),
+        ('enhance_cognitive_function', 'Enhance Cognitive Function'),
+        ('promote_physical_wellness', 'Promote Physical Wellness'),
+        ('facilitate_emotional_recovery', 'Facilitate Emotional Recovery'),
+        ('support_social_integration', 'Support Social Integration'),
+    ]
+
+    DETERMINATION_LEVEL_CHOICES = [
+        ('casual', 'Casual'),
+        ('determined', 'Determined'),
+        ('very_determined', 'Very Determined'),
+    ]
+
+    # Fields
+
     username = models.CharField(max_length=255, unique=True) # must be unique an < 255 characters
     email = models.EmailField(max_length=255, unique=True) # must be unique an < 255 characters
     brain_injury_details = models.TextField(null=True, blank=True) # can be null and blank
